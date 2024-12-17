05714dd268dccf2c176d97f04d368caaca822625
Lautaro Massara <62029472+RkTcaster@users.noreply.github.com>
2024-12-17 13:21:38 -0300

    add new player disable fuction (#10)

--- src/swiss/views/Tornament/PlayerForm/PlayerSelect/index.tsx
+++ src/swiss/views/Tornament/PlayerForm/PlayerSelect/index.tsx
@@ -1,43 +1,34 @@
 import Icon from '@/src/swiss/components/Icon'
-import Input from '@/src/swiss/components/Input'
 import css from './style.module.css'
 import { useState } from 'react'
-import { Player } from '@/services/lib/definitions'
 
 type Props = {
   index: number
   inputValue: string
   removePlayer: (index: number) => void
-  inputProps?: React.InputHTMLAttributes<HTMLInputElement>
-  fetchedPlayers : string[]
-  handlePlayerNameChange: ({ name, index }: { name: string; index: number }) => void;
+  fetchedPlayers: string[]
+  handlePlayerNameChange: ({ name, index }: { name: string; index: number }) => void
   selectedPlayers: string[]
 }
 
-
-const PlayerSelectField = ({ inputValue, index, removePlayer, inputProps, fetchedPlayers, handlePlayerNameChange, selectedPlayers }: Props) => {
-
-  const fetchedPlayersArray = fetchedPlayers.map((fetchedPlayer) => {return fetchedPlayer})
-  const [options, setOptions] = useState<string[]>(fetchedPlayersArray)
-  const [selectedOption,setSelectedOption] = useState<string | null>(null)
-  const [newOption, setNewOption] = useState<string>("")
-
-  const handleAddOption = () => {
-    if (newOption.trim() && !options.includes(newOption)) {
-      setOptions((prev) => [...prev, newOption]);
-      setSelectedOption(newOption);
-      setNewOption("");
-  }}
+const PlayerSelectField = ({
+  inputValue,
+  index,
+  removePlayer,
+  fetchedPlayers,
+  handlePlayerNameChange,
+  selectedPlayers,
+}: Props) => {
+  const [selectedOption, setSelectedOption] = useState<string | null>(null)
 
   return (
     <div key={`${inputValue}${index}`}>
-              <select
-              
-        id="custom-select"
-        value={inputValue || ""}
+      <select
+        id='custom-select'
+        value={inputValue || ''}
         onChange={(e) => (setSelectedOption(e.target.value), handlePlayerNameChange({ name: e.target.value, index }))}
       >
-        <option value="" disabled>
+        <option value='' disabled>
           Choose a player
         </option>
         {fetchedPlayers.map((player, idx) => (
@@ -46,7 +37,7 @@ const PlayerSelectField = ({ inputValue, index, removePlayer, inputProps, fetche
           </option>
         ))}
       </select>
-      
+
       <Icon
         name='Close'
         className={css.icon}
